d90f7f49c48f5eaeb9383b49b86f8b0b69d9414f
Jeff Williams <jeffwilliams824@gmail.com>
2025-08-12 12:01:19 +0000

    fix: getting function app to work locally

--- EmailNotionSync/EmailNotionSync.AppHost/AppHost.cs
+++ EmailNotionSync/EmailNotionSync.AppHost/AppHost.cs
@@ -7,9 +7,6 @@ var notionApi = builder.AddProject<Projects.EmailNotionSync_NotionApi>("notionap
 
 builder.AddProject<Projects.EmailNotionSync_FunctionApp>("functionapp")
     .WithReference(gmailApi)
-    .WithReference(notionApi)
-    .WithEnvironment("ASPNETCORE_URLS", "http://localhost:18888")
-    .WithEnvironment("ASPIRE_DASHBOARD_OTLP_HTTP_ENDPOINT_URL", "http://localhost:4318")
-    .WithEnvironment("ASPIRE_ALLOW_UNSECURED_TRANSPORT", "true");
+    .WithReference(notionApi);
 
 builder.Build().Run();
