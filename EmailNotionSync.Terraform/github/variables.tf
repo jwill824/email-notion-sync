dd7c50d64203c4784692f552590df0d23ae34fda
Jeff Williams <jeffwilliams824@gmail.com>
2025-08-24 19:10:45 +0000

    chore: updating tf

--- EmailNotionSync.Terraform/github/variables.tf
+++ EmailNotionSync.Terraform/github/variables.tf
@@ -19,12 +19,6 @@ variable "function_app_name" {
   type        = string
 }
 
-variable "azure_credentials" {
-  description = "Azure credentials for login action (JSON)"
-  type        = string
-  sensitive   = true
-}
-
 variable "azure_gmailapi_app_name" {
   description = "Azure GmailApi Container App name"
   type        = string
@@ -37,5 +31,20 @@ variable "azure_notionapi_app_name" {
 
 variable "azure_resource_group" {
   description = "Azure Resource Group name"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "Azure AD App (client) ID for OIDC federation"
+  type        = string
+}
+
+variable "azure_tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "azure_subscription_id" {
+  description = "Azure subscription ID"
   type        = string
 }
